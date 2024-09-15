49cddaf7472fb88f75cd211fe3a09f66d95d7f78
code4hari <Harisanku7@gmail.com>
2024-09-15 04:34:03 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,51 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+import logging
+import os
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+logging.basicConfig(level=logging.INFO)
 
-@app.route('/about')
-def about():
-    return 'About'
+DOWNLOAD_DIR = 'danceteacher/frontend/src/assets/media'
+
+
+def download_video(url):
+    ydl_opts = {
+        'verbose': True,  
+        'restrictfilenames': True, 
+        'outtmpl': os.path.join(DOWNLOAD_DIR, '%(title)s-%(id)s.%(ext)s'),
+    }
+    
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            return info['title']
+    except yt_dlp.utils.DownloadError as e:
+        logging.error(f"Download error: {e}")
+        return None
+
+
+@app.route('/download', methods=['POST'])
+def download_and_host_video():
+    url = request.json['url']
+    video_title = download_video(url)
+    
+    if video_title:
+        return jsonify({'message': f'Video "{video_title}" downloaded successfully'})
+    else:
+        return jsonify({'error': 'Failed to download video'}), 400
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == '__main__':
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    
+    app.run(debug=True)
